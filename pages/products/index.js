187bfe1d4acc37e0e908cb5ce5a9f5cfbfb8cee9
hadi499 <hadi.ygcl@gmail.com>
2023-04-06 03:57:53 +0700

    get-static-props

---
+++ pages/products/index.js
@@ -0,0 +1,27 @@
+const ProductList = ({ products }) => {
+  return (
+    <div>
+      {products.map((item) => (
+        <ul>
+          <li>
+            {item.name} - {item.price}
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:5000/products");
+  const data = await response.json();
+  console.log(data);
+
+  return {
+    props: {
+      products: data,
+    },
+  };
+};
